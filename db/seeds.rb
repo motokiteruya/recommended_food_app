5a03a075ead74bb8803e673ba991d410b1651942
motoki <81569237+motokiteruya@users.noreply.github.com>
2021-05-23 21:45:07 +0900

    詳細表示機能

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,18 @@
-User.create!(email: "test@example.com", password: "password")
+email = "test@example.com"
+password = "password"
+
+ActiveRecord::Base.connection.execute("TRUNCATE TABLE foods RESTART IDENTITY CASCADE")
+
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user2.foods.create!(name: "沖縄そば", comment: "沖縄そばです。\n美味しいです。")
+user1.foods.create!(name: "海ぶどう", comment: "海ぶどうです。\n美味しいです。")
+user3.foods.create!(name: "ゴーヤーチャンプルー", comment: "ゴーヤーチャンプルーです。\n美味しいです。")
+user3.foods.create!(name: "サーターアンダギー", comment: "サーターアンダギーです。\n美味しいです。")
+user1.foods.create!(name: "タコライス", comment: "タコライスです。\n美味しいです。")
+
+User.create!(email: email, password: password)
+
 puts "ユーザーの初期データインポートに成功しました。"
